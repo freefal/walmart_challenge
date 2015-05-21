9b1feffecdf8414134642d3c3e1670fa744215d7
Mark Henle <freefal67@gmail.com>
2015-05-21 17:32:59 -0400

    Now running regressions.

--- src/Attempt1.java
+++ src/Attempt1.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.text.*;
 import java.util.*;
+import org.apache.commons.math3.stat.regression.*;
+import org.apache.commons.math3.linear.*;
+import org.apache.commons.lang3.*;
 
 public class Attempt1 {
 	static ArrayList<SalesDataPoint> salesData;
@@ -11,7 +14,7 @@ public class Attempt1 {
 	static HashMap<String, WeatherDataPoint> weatherMap;
 	static int storeCount;
 	static int itemCount;
-
+	
 	public static void main(String[] args) {
 		try {
 			salesData = readSalesData(args[0]);
@@ -23,17 +26,48 @@ public class Attempt1 {
 		keyMap = createKeyLookup(keyData);
 		weatherMap = createWeatherLookup(weatherData);
 		
-		System.out.println("All data read");
+		System.out.println("Going to Attach Weather");
 		attachWeatherData(salesData, weatherData, keyData);
-		System.out.println("Weather attached");
+		System.out.println("Attached Weather");
 		
 		OLSInput[][] olsinputs = new OLSInput[storeCount][itemCount];
 		
-		for (int i = 0; i < olsinputs.length; i++)
-			for (int j = 0; j < olsnputs[i].length; j++) {
-				
+		for (int i = 0; i < olsinputs.length; i++) {
+			for (int j = 0; j < olsinputs[i].length; j++) {
+				olsinputs[i][j] = new OLSInput();
 			}
+		}
+	
+		System.out.println("Going to set up OLSInputs");
 		
+		for (SalesDataPoint sdp : salesData) {
+			OLSInput oi = olsinputs[sdp.store - 1][sdp.item - 1];
+			double precipitation = 0.0;
+			WeatherDataPoint wdp = sdp.todayWeather;
+			precipitation = (wdp.snowfall > 0.0 || wdp.preciptotal > 0.0) ? 1.0 : 0.0;
+			oi.add(sdp.units, precipitation);
+		}
+		salesData = null;
+		weatherData = null;
+		keyData = null;
+		keyMap = null;
+		
+		System.out.println("Set up OLSInputs");
+
+		double[][][] betas = new double[storeCount][itemCount][2];
+		for (int i = 0; i < olsinputs.length; i++) {
+			for (int j = 0; j < olsinputs[i].length; j++) {
+				double[] y = ArrayUtils.toPrimitive(olsinputs[i][j].y.toArray(new Double[0]));
+				double[][] x = new double[2][olsinputs[i][j].x1.size()];
+				x[0] = ArrayUtils.toPrimitive(olsinputs[i][j].x1.toArray(new Double[0]));
+				x[1] = ArrayUtils.toPrimitive(olsinputs[i][j].x2.toArray(new Double[0]));
+				
+				OLSMultipleLinearRegression ols = new OLSMultipleLinearRegression ();
+				ols.newSampleData(y, x);
+				betas[i][j] = ols.estimateRegressionParameters();
+			}
+		}
+		olsinputs = null;
 	}
 
 	public static ArrayList<SalesDataPoint> readSalesData (String filename) throws Exception {
@@ -106,18 +140,12 @@ public class Attempt1 {
 	}
 	
 	public static void attachWeatherData (ArrayList<SalesDataPoint> salesData, ArrayList<WeatherDataPoint> weatherData, ArrayList<KeyDataPoint> keyData) {
-		int count = 0;
-		int size = salesData.size();
 		for (SalesDataPoint sdp : salesData) {
 			int station = keyMap.get(sdp.store);
 			WeatherDataPoint todayWeather = weatherMap.get(station + "-" + sdp.date);
 			//Date tomorrowDate = new Date(date.
 			//WeatherDataPoint tomorrowWeather = weatherMap.get(station + "-" + tomorrowDate);
 			sdp.todayWeather = todayWeather;
-			count++;
-			if ((count % 1000) == 0) {
-				System.out.println(count + " of " + size);
-			}
 		}
 	}
 }
--- src/OLSInput.java
+++ src/OLSInput.java
@@ -3,13 +3,19 @@ import java.util.*;
 
 
 public class OLSInput {
+	ArrayList<Double> y;
 	ArrayList<Double> x1;
 	ArrayList<Double> x2;
-	ArrayList<Double> y;
-	
-	public OLSInput (y, x1, x2) {
+
+	public OLSInput () {
+		y = new ArrayList<Double>();
+		x1 = new ArrayList<Double>();
+		x2 = new ArrayList<Double>();
+	}
+
+	public void add (double y, double x2) {
 		this.y.add(y);
-		this.x1.add(x1);
+		this.x1.add(1.0);
 		this.x2.add(x2);
 	}
 }
